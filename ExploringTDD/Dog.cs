bb475d9ea5c44d7d0fa209158d6ebf23069cb1be
LandoB <jrolandobernal@gmail.com>
2015-07-15 19:44:53 -0500

    Initial commit

---
+++ ExploringTDD/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExploringTDD
+{
+	public interface IDog
+	{
+		string Bark ();
+		string Whine ();
+		string Scratch();
+
+	}
+
+	public class Yorkie : IDog
+	{
+		public virtual string Bark ()
+		{
+			return "Yip";
+		}
+
+		public virtual string Whine ()
+		{
+			return "Ugh";
+		}
+
+		public virtual string Scratch ()
+		{
+			return "Itch";
+		}
+	}
+}
+
---
+++ TestExploringTDD/Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using ExploringTDD; // This SHOULD match the name of the OTHER project
+
+namespace TestExploringTDD
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestHowYorkieBarks ()
+		{
+			Yorkie my_dog = new Yorkie(); 
+
+			Assert.AreEqual("Yip",my_dog.Bark()); // We tried Assert.Equals but it says it shouldn't be used.
+		}
+
+		[Test ()]
+		public void TestHowYorkieScratches ()
+		{
+			Yorkie my_dog = new Yorkie(); 
+
+			Assert.AreEqual("Itch",my_dog.Scratch()); // We tried Assert.Equals but it says it shouldn't be used.
+		}
+
+
+		[Test ()]
+		public void TestHowYorkieWhines ()
+		{
+			Yorkie my_dog = new Yorkie(); 
+
+			Assert.AreEqual("Ugh",my_dog.Whine()); // We tried Assert.Equals but it says it shouldn't be used.
+		}
+
+
+	}
+}
+
